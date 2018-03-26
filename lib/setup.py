d7da71f94655917dcca324c9f582511a4dfc0d88
ninehb <37722210+ninehb@users.noreply.github.com>
2018-03-26 10:27:44 -0400

    Update setup.py

--- lib/setup.py
+++ lib/setup.py
@@ -11,6 +11,8 @@ import numpy as np
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
+nvcc_bin = 'nvcc.exe'
+lib_dir = 'lib/x64'
 
 def find_in_path(name, path):
     "Find a file in a search path"
@@ -32,21 +34,22 @@ def locate_cuda():
     """
 
     # first check if the CUDAHOME env variable is in use
-    if 'CUDAHOME' in os.environ:
-        home = os.environ['CUDAHOME']
-        nvcc = pjoin(home, 'bin', 'nvcc')
+    if 'CUDA_PATH' in os.environ:
+        home = os.environ['CUDA_PATH']
+        print("home = %s\n" % home)
+        nvcc = pjoin(home, 'bin', nvcc_bin)
     else:
         # otherwise, search the PATH for NVCC
         default_path = pjoin(os.sep, 'usr', 'local', 'cuda', 'bin')
-        nvcc = find_in_path('nvcc', os.environ['PATH'] + os.pathsep + default_path)
+        nvcc = find_in_path(nvcc_bin, os.environ['PATH'] + os.pathsep + default_path)
         if nvcc is None:
             raise EnvironmentError('The nvcc binary could not be '
-                'located in your $PATH. Either add it to your path, or set $CUDAHOME')
+                'located in your $PATH. Either add it to your path, or set $CUDA_PATH')
         home = os.path.dirname(os.path.dirname(nvcc))
 
     cudaconfig = {'home':home, 'nvcc':nvcc,
                   'include': pjoin(home, 'include'),
-                  'lib64': pjoin(home, 'lib64')}
+                  'lib64': pjoin(home, lib_dir)}
     for k, v in cudaconfig.items():
         if not os.path.exists(v):
             raise EnvironmentError('The CUDA %s path could not be located in %s' % (k, v))
@@ -71,32 +74,34 @@ def customize_compiler_for_nvcc(self):
     subclassing going on."""
 
     # tell the compiler it can processes .cu
-    self.src_extensions.append('.cu')
+    #self.src_extensions.append('.cu')
 
     # save references to the default compiler_so and _comple methods
-    default_compiler_so = self.compiler_so
-    super = self._compile
+    #default_compiler_so = self.spawn 
+    #default_compiler_so = self.rc
+    super = self.compile
 
     # now redefine the _compile method. This gets executed for each
     # object but distutils doesn't have the ability to change compilers
     # based on source extension: we add it.
-    def _compile(obj, src, ext, cc_args, extra_postargs, pp_opts):
-        print(extra_postargs)
-        if os.path.splitext(src)[1] == '.cu':
+    def compile(sources, output_dir=None, macros=None, include_dirs=None, debug=0, extra_preargs=None, extra_postargs=None, depends=None):
+        postfix=os.path.splitext(sources[0])[1]
+        
+        if postfix == '.cu':
             # use the cuda for .cu files
-            self.set_executable('compiler_so', CUDA['nvcc'])
+            #self.set_executable('compiler_so', CUDA['nvcc'])
             # use only a subset of the extra_postargs, which are 1-1 translated
             # from the extra_compile_args in the Extension class
             postargs = extra_postargs['nvcc']
         else:
             postargs = extra_postargs['gcc']
 
-        super(obj, src, ext, cc_args, postargs, pp_opts)
+        return super(sources, output_dir, macros, include_dirs, debug, extra_preargs, postargs, depends)
         # reset the default compiler_so, which we might have changed for cuda
-        self.compiler_so = default_compiler_so
+        #self.compiler_so = default_compiler_so
 
     # inject our redefined _compile method into the class
-    self._compile = _compile
+    self.compile = compile
 
 # run the customize_compiler
 class custom_build_ext(build_ext):
@@ -107,33 +112,37 @@ class custom_build_ext(build_ext):
 ext_modules = [
     Extension(
         "utils.cython_bbox",
-        ["utils/bbox.pyx"],
-        extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
+        sources=["utils\\bbox.pyx"],
+        #define_macros={'/LD'},
+        #extra_compile_args={'gcc': ['/link', '/DLL', '/OUT:cython_bbox.dll']},
+        #extra_compile_args={'gcc': ['/LD']},
+        extra_compile_args={'gcc': []},
         include_dirs = [numpy_include]
     ),
     Extension(
         "nms.cpu_nms",
-        ["nms/cpu_nms.pyx"],
-        extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
-        include_dirs = [numpy_include]
+        sources=["nms\\cpu_nms.pyx"],
+        #extra_compile_args={'gcc': ["-Wno-cpp", "-Wno-unused-function"]},
+        extra_compile_args={'gcc': []},
+        include_dirs = [numpy_include],
     ),
-    Extension('nms.gpu_nms',
-        ['nms/nms_kernel.cu', 'nms/gpu_nms.pyx'],
-        library_dirs=[CUDA['lib64']],
-        libraries=['cudart'],
-        language='c++',
-        runtime_library_dirs=[CUDA['lib64']],
+    #Extension('nms.gpu_nms',
+    #    ['nms/nms_kernel.cu', 'nms/gpu_nms.pyx'],
+    #    library_dirs=[CUDA['lib64']],
+    #    libraries=['cudart'],
+    #    language='c++',
+    #    runtime_library_dirs=[CUDA['lib64']],
         # this syntax is specific to this build system
         # we're only going to use certain compiler args with nvcc and not with gcc
         # the implementation of this trick is in customize_compiler() below
-        extra_compile_args={'gcc': ["-Wno-unused-function"],
-                            'nvcc': ['-arch=sm_52',
-                                     '--ptxas-options=-v',
-                                     '-c',
-                                     '--compiler-options',
-                                     "'-fPIC'"]},
-        include_dirs = [numpy_include, CUDA['include']]
-    )
+    #    extra_compile_args={'gcc': ["-Wno-unused-function"],
+    #                        'nvcc': ['-arch=sm_52',
+    #                                 '--ptxas-options=-v',
+    #                                 '-c',
+    #                                 '--compiler-options',
+    #                                 "'-fPIC'"]},
+    #    include_dirs = [numpy_include, CUDA['include']]
+    #)
 ]
 
 setup(
